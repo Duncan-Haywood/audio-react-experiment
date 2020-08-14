d50c0bc4036ca023e16daa4405d6d46f5fabadaf
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-13 19:27:46 -0700

    touches up audio files for sound fx; incorporates water drop button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 // import { Howl, Howler } from 'howler'
 // import cricketsAudio from './sounds/crickets-night.mp3'
 import menuClickAduio from './sounds/menu-button-click.wav'
+import waterDropAudio from './sounds/water-drop-click-production.mp3'
 import UIfx from 'uifx'
 
 
 
 class App extends React.Component {
-  beep = new UIfx(menuClickAduio);
-  playBeep = () => {this.beep.play(1.0)}
-
+  click = new UIfx(menuClickAduio);
+  waterDrop = new UIfx(waterDropAudio);
+  playClick = () => {this.click.play(1.0)}
+  playWaterDrop = () => {this.waterDrop.play(1.0)}
   render() {
     return (
       <div className="App">
@@ -20,7 +22,8 @@ class App extends React.Component {
           <p>
             A React app for testing how to play audio.
           </p>
-          <button onClick={this.playBeep}>Make a Click</button>
+          <button onClick={this.playClick}>Make a Click</button>
+          <button onClick={this.playWaterDrop}>Water Drop</button>
         </header>
       </div>
     );
