4c214cc57972b0a0230a3f0bfd3d7e217ead2d12
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-15 15:27:55 -0700

    adds ambient cricket sounds; TODO: toggle on/off option

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import { Howl, Howler } from 'howler'
-// import cricketsAudio from './sounds/crickets-night.mp3'
+import { Howl, Howler } from 'howler'
+import cricketsAudio from './sounds/crickets-night.mp3'
 import menuClickAudio from './sounds/menu-button-click.wav'
 import waterDropAudio from './sounds/water-drop-click-production.mp3'
 import UIfx from 'uifx'
-
 
 
 class App extends React.Component {
@@ -14,6 +13,15 @@ class App extends React.Component {
   waterDrop = new UIfx(waterDropAudio);
   playClick = () => {this.click.play(1.0)}
   playWaterDrop = () => {this.waterDrop.play(1.0)}
+  
+  crickets = new Howl({
+    src: cricketsAudio,
+    autoplay: true,
+    loop: true,
+    volume: 0.1
+    })
+  playCrickets = () => {this.crickets.play()}
+
   render() {
     return (
       <div className="App">
@@ -22,6 +30,7 @@ class App extends React.Component {
           <p>
             A React app for testing how to play audio.
           </p>
+          {/*<button onClick={this.playCrickets}>Play Crickets</button>*/}
           <button onClick={this.playClick}>Make a Click</button>
           <button onClick={this.playWaterDrop}>Water Drop</button>
         </header>
