0bdad76a37639193b59a5a0d2129879dca256c6d
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-15 15:09:18 -0700

    fixes typo

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 // import { Howl, Howler } from 'howler'
 // import cricketsAudio from './sounds/crickets-night.mp3'
-import menuClickAduio from './sounds/menu-button-click.wav'
+import menuClickAudio from './sounds/menu-button-click.wav'
 import waterDropAudio from './sounds/water-drop-click-production.mp3'
 import UIfx from 'uifx'
 
 
 
 class App extends React.Component {
-  click = new UIfx(menuClickAduio);
+  click = new UIfx(menuClickAudio);
   waterDrop = new UIfx(waterDropAudio);
   playClick = () => {this.click.play(1.0)}
   playWaterDrop = () => {this.waterDrop.play(1.0)}
