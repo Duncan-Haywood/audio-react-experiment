e25dad858d4d7f20b08948d76d29511e546c3c5d
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-15 17:10:00 -0700

    factors out sound code into a seperate class; adds play-pause functionality to background noise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Howl, Howler } from 'howler'
+import { Howl } from 'howler'
 import cricketsAudio from './sounds/crickets-night.mp3'
 import menuClickAudio from './sounds/menu-button-click.wav'
 import waterDropAudio from './sounds/water-drop-click-production.mp3'
 import UIfx from 'uifx'
 
 
-class App extends React.Component {
+class Sounds extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {playing: true};
+  }
+
   click = new UIfx(menuClickAudio);
   waterDrop = new UIfx(waterDropAudio);
   playClick = () => {this.click.play(1.0)}
@@ -16,12 +21,35 @@ class App extends React.Component {
   
   crickets = new Howl({
     src: cricketsAudio,
-    autoplay: true,
+    autoplay: false,
     loop: true,
     volume: 0.1
     })
-  playCrickets = () => {this.crickets.play()}
 
+  playCrickets = () => {this.crickets.play()}
+  pauseCrickets = () => {this.crickets.pause()}
+  toggleCrickets = () => {this.state.playing ? this.pauseCrickets(): this.playCrickets(); 
+    this.setState({playing :! this.state.playing})
+  }
+  componentDidMount() {
+    this.playCrickets()
+  }
+  render(){
+    return(
+      <div>
+      <button onClick={this.toggleCrickets}>{this.state.playing ? "Pause Crickets":"Play Crickets"}</button>
+      <button onClick={this.playClick}>Make a Click</button>
+      <button onClick={this.playWaterDrop}>Water Drop</button>
+      </div>
+    )
+  
+  }
+
+}
+
+
+class App extends React.Component {
+  
   render() {
     return (
       <div className="App">
@@ -30,9 +58,7 @@ class App extends React.Component {
           <p>
             A React app for testing how to play audio.
           </p>
-          {/*<button onClick={this.playCrickets}>Play Crickets</button>*/}
-          <button onClick={this.playClick}>Make a Click</button>
-          <button onClick={this.playWaterDrop}>Water Drop</button>
+          <Sounds/>
         </header>
       </div>
     );
