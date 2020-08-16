0bc6e95a5a91ac42af49f64c38af72e46720310c
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-15 17:41:23 -0700

    refactors sounds into more classes, seperating purpose into "Doing one thing per place"

--- src/App.js
+++ src/App.js
@@ -7,18 +7,30 @@ import menuClickAudio from './sounds/menu-button-click.wav'
 import waterDropAudio from './sounds/water-drop-click-production.mp3'
 import UIfx from 'uifx'
 
-
-class Sounds extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {playing: true};
-  }
-
+// function SoundsDev(){
+//   const [playing, setPlaying] = useState(true);
+  
+//   return();
+// }
+class SoundFX extends React.Component{
   click = new UIfx(menuClickAudio);
   waterDrop = new UIfx(waterDropAudio);
   playClick = () => {this.click.play(1.0)}
   playWaterDrop = () => {this.waterDrop.play(1.0)}
-  
+  render(){
+    return(
+      <React.Fragment>
+        <button onClick={this.playClick}>Make a Click</button>
+        <button onClick={this.playWaterDrop}>Water Drop</button>
+      </React.Fragment>
+    );
+  }
+}
+class BackgroundSound extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {playing: true};
+  }
   crickets = new Howl({
     src: cricketsAudio,
     autoplay: false,
@@ -36,12 +48,19 @@ class Sounds extends React.Component{
   }
   render(){
     return(
-      <div>
       <button onClick={this.toggleCrickets}>{this.state.playing ? "Pause Crickets":"Play Crickets"}</button>
-      <button onClick={this.playClick}>Make a Click</button>
-      <button onClick={this.playWaterDrop}>Water Drop</button>
-      </div>
-    )
+      );
+  }
+}
+class Sounds extends React.Component{
+  
+  render(){
+    return(
+      <React.Fragment>
+        <BackgroundSound />
+        <SoundFX />
+      </React.Fragment>
+    );
   
   }
 
@@ -58,7 +77,9 @@ class App extends React.Component {
           <p>
             A React app for testing how to play audio.
           </p>
-          <Sounds/>
+          <div>
+            <Sounds/>
+          </div>
         </header>
       </div>
     );
